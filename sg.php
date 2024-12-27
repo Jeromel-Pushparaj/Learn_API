980cde83e81df519285301662c4a81da84b3f24a
Jeromel Pushparaj <jeromal03@gmail.com>
2024-12-27 14:35:29 +0530

    Lap Setup Completed

--- sg.php
+++ sg.php
@@ -9,6 +9,12 @@ print_r($_GET);
 print_r($_FILES);
 print_r($_ENV);
 print_r($_COOKIE);
-
+print("This is the test");
+$config_json = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/../env.json');
+print_r(json_decode($config_json, true));
+if(file_exists($_SERVER['DOCUMENT_ROOT'].'/.. env.json')){
+  print("Hello");
+}
+print(file_get_contents("/var/www/env.json"));
 ?>
 </pre>
